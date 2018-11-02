0b21333a0a896261564aa99922171584b7694e57
Carlos <carcarbe@gmail.com>
2018-11-02 11:15:34 +0100

    Read temperature working on raspberry pi sense hat.

--- ConsoleDaemonProducer/Program.cs
+++ ConsoleDaemonProducer/Program.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Sense.Led;
+using Sense.RTIMU;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleDaemonProducer
 {
@@ -11,7 +16,6 @@ namespace ConsoleDaemonProducer
     {
         public static async Task Main(string[] args)
         {
-            
 
             var builder = new HostBuilder()
     .ConfigureAppConfiguration((hostingContext, config) =>
@@ -35,7 +39,68 @@ namespace ConsoleDaemonProducer
         logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
         logging.AddConsole();
     });
-            Console.WriteLine("Hello World!");
+
+
+            using (var settings = RTIMUSettings.CreateDefault())
+            using (var imu = settings.CreateIMU())
+            using (var pressure = settings.CreatePressure())
+            using (var humidity = settings.CreateHumidity())
+            {
+
+                var imuData = imu.GetData();
+                Console.WriteLine($"Timestamp: {imuData.Timestamp:O}");
+                Console.WriteLine($"FusionPose: Valid: {imuData.FusionPoseValid}, Value: {imuData.FusionPose}");
+                Console.WriteLine($"FusionQPose: Valid: {imuData.FusionQPoseValid}, Value: {imuData.FusionQPose}");
+                Console.WriteLine($"Gyro: Valid: {imuData.GyroValid}, Value: {imuData.Gyro}");
+                Console.WriteLine($"Accel: Valid: {imuData.AccelValid}, Value: {imuData.Accel}");
+                Console.WriteLine($"Compass: Valid: {imuData.CompassValid}, Value: {imuData.Compass}");
+                Console.WriteLine();
+
+                var pressureReadResult = pressure.Read();
+                Console.WriteLine($"Pressure valid: {pressureReadResult.PressureValid}");
+                Console.WriteLine($"Pressure: {pressureReadResult.Pressure}");
+                Console.WriteLine($"Temperature valid: {pressureReadResult.TemperatureValid}");
+                Console.WriteLine($"Temperature: {pressureReadResult.Temperatur}");
+                Console.WriteLine();
+
+                var humidityReadResult = humidity.Read();
+                Console.WriteLine($"Humidity valid: {humidityReadResult.HumidityValid}");
+                Console.WriteLine($"Humidity: {humidityReadResult.Humidity}");
+                Console.WriteLine($"Temperature valid: {humidityReadResult.TemperatureValid}");
+                Console.WriteLine($"Temperature: {humidityReadResult.Temperatur}");
+
+                Console.WriteLine("===================================================");
+
+            }
+
+    //        var pixels = Sense.Led.PixelsFromText
+    //.Create("A")
+    //.SetColor(new Color(255, 255, 255));
+    //        Sense.Led.LedMatrix.SetPixels(pixels);
+    //        while (true)
+    //        {
+    //            Sense.Led.LedMatrix.SetLowLight(true);
+    //            System.Console.WriteLine("LowLight = true");
+    //            System.Console.WriteLine($"Gamma: {string.Join(" ", Sense.Led.LedMatrix.GetGamma().Select(v => v.ToString("X")))}");
+    //            Thread.Sleep(2000);
+
+    //            Sense.Led.LedMatrix.SetLowLight(false);
+    //            System.Console.WriteLine("LowLight = false");
+    //            System.Console.WriteLine($"Gamma: {string.Join(" ", Sense.Led.LedMatrix.GetGamma().Select(v => v.ToString("X")))}");
+    //            Thread.Sleep(2000);
+
+    //            var buffer = new byte[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 10, 10 };
+    //            Sense.Led.LedMatrix.SetGamma(buffer);
+    //            System.Console.WriteLine("Light = custom");
+    //            System.Console.WriteLine($"Gamma: {string.Join(" ", Sense.Led.LedMatrix.GetGamma().Select(v => v.ToString("X")))}");
+    //            Thread.Sleep(2000);
+
+    //            Sense.Led.LedMatrix.SetLowLight(false);
+    //            System.Console.WriteLine("LowLight = false");
+    //            System.Console.WriteLine($"Gamma: {string.Join(" ", Sense.Led.LedMatrix.GetGamma().Select(v => v.ToString("X")))}");
+    //            Thread.Sleep(2000);
+    //        }
+
             await builder.RunConsoleAsync();
         }
     }

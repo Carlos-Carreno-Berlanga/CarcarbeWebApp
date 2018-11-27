89418327b5d26b117adcfc63d6d221fc6f605c9a
carcaber <carcaber@globalomnium.com>
2018-11-27 14:11:16 +0100

    Add basic chart.

--- CarcarbeWebApp/ClientApp/src/components/Measurements.js
+++ CarcarbeWebApp/ClientApp/src/components/Measurements.js
@@ -3,26 +3,70 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { actionCreators } from '../store/Measurements';
+import Plot from 'react-plotly.js';
 
 class Measurements extends Component {
 
     componentWillMount() {
-                
+
         this.props.requestMeasurements('pressure');
     }
 
 
-  render() {
-    return (
-      <div>
-        <h1>measurementsPage</h1>
- 
-      </div>
-    );
-  }
+    render() {
+        let x = [];
+        let y = [];
+        let cont = 0;
+        this.props.measurements.forEach(measurement => { x.push(measurement.timeStamp); cont++; y.push(measurement.value); });
+
+        return (
+            <div>
+                <h1>measurementsPage</h1>
+                <Plot
+                    data={[
+                        {
+                            x,
+                            y,
+                            type: 'scatter',
+                            mode: 'lines',
+                            marker: { color: 'red' },
+                        },
+                    ]}
+                    layout={{
+                        width: 720, height: 740,
+
+                        title: 'A Fancy Plot',
+                        xaxis: {
+                            autorange: true,
+                            range: [x[0], x[x.length - 1]],
+                            rangeselector: {
+                                buttons: [
+                                    {
+                                        count: 1,
+                                        label: '1d',
+                                        step: 'day',
+                                        stepmode: 'backward'
+                                    },
+                                    {
+                                        count: 6,
+                                        label: '6d',
+                                        step: 'day',
+                                        stepmode: 'backward'
+                                    },
+                                    { step: 'all' }
+                                ]
+                            },
+                            rangeslider: { range: [x[0], x[x.length - 1]] },
+                            type: 'date'
+                        },
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 export default connect(
     state => state.measurementsPage,
-  dispatch => bindActionCreators(actionCreators, dispatch)
+    dispatch => bindActionCreators(actionCreators, dispatch)
 )(Measurements);

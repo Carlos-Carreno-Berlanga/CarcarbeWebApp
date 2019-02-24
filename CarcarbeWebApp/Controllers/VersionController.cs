9f35c0fa2d30cedf4c273698af4640fadac2818a
Carlos <carcarbe@gmail.com>
2019-02-24 13:24:10 +0100

    Upgrade .net core version and add test certicate in order to test https.

---
+++ CarcarbeWebApp/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcarbeWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet, ]
+        public  IActionResult GetVersion()
+        {
+           
+            return Ok("HOLA");
+        }
+    }
+}

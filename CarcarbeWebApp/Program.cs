cf70ec104a786e148f08c5fb490211336e23d998
Carlos <carcarbe@gmail.com>
2019-11-14 22:06:58 +0100

    Enable http2

--- CarcarbeWebApp/Program.cs
+++ CarcarbeWebApp/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System;
+using System.Net;
 
 namespace CarcarbeWebApp
 {
@@ -12,6 +15,25 @@ namespace CarcarbeWebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                    .ConfigureKestrel(options =>
+                    {
+                        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                        {
+                            options.ListenLocalhost(5001, listenOptions =>
+                            {
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                                listenOptions.UseHttps("carcarbe.pfx", Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password"));
+                            });
+                        }
+                        else
+                        {
+                            options.Listen(IPAddress.Any, 5001, listenOptions =>
+                            {
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                                listenOptions.UseHttps("carcarbe.pfx", Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password"));
+                            });
+                        }
+                    })
                 .UseStartup<Startup>();
     }
 }
--- ConsoleDaemonProducer/Program.cs
+++ ConsoleDaemonProducer/Program.cs
@@ -25,6 +25,7 @@ namespace ConsoleDaemonProducer
             var builder = new HostBuilder()
     .ConfigureAppConfiguration((hostingContext, config) =>
     {
+ 
         config.AddEnvironmentVariables();
 
         if (args != null)
@@ -49,7 +50,7 @@ namespace ConsoleDaemonProducer
         services.AddRebus(configure => configure
     .Logging(l => l.Use(new MSLoggerFactoryAdapter(loggerFactory)))
     .Transport(t => t.UseRabbitMqAsOneWayClient("amqp://pklfurgc:4YJosxjltR4AntkkvVignFH-TKW16c9k@raven.rmq.cloudamqp.com/pklfurgc")
-    .InputQueueOptions((cfg => { cfg.SetDurable(true); cfg.SetAutoDelete(false, 600000);  }))
+    .InputQueueOptions((cfg => { cfg.SetDurable(true); cfg.SetAutoDelete(false, 600000); }))
     
     )
     .Routing(r => r.TypeBased().Map<MeterMessage>("messages-queue"))
